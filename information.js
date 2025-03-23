e07bad7a3bf77742196003d0ff250a6d79034b2d
Nicholas Lim <theonlineservices@gmail.com>
2025-03-23 12:46:36 -0700

    Update information.js

--- information.js
+++ information.js
@@ -1,6 +1,6 @@
 sessionStorage.setItem("informationLoaded", "false");
 
-    var informationHTML = `<center><div id="version" style="display: inline-block;"></div>
+    var informationHTML = `<center><div id="version" style="display: inline-block;"></div><BR><BR>
 <p>
 © 2025 Internet Services LLC | AveMariaCloud.com<BR><BR>
 <a href="mailto:website@avemariacloud.com">website@avemariacloud.com</a>
