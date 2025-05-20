85df924fa748f20d698214022f067c96926b1412
Nicholas Lim <theonlineservices@gmail.com>
2025-05-20 10:20:06 -0700

    Update update-models.js

--- update-models.js
+++ update-models.js
@@ -7,24 +7,17 @@ var models = [
 'meta-llama/llama-4-maverick-17b-128e-instruct',
 'meta-llama/llama-4-scout-17b-16e-instruct',
 'deepseek-r1-distill-llama-70b',
-'deepseek-r1-distill-qwen-32b',
 'gemma2-9b-it',
 'llama-3.1-8b-instant',
-'llama-3.2-11b-vision-preview',
-'llama-3.2-1b-preview',
-'llama-3.2-3b-preview',
-'llama-3.2-90b-vision-preview',
-'llama-3.3-70b-specdec',
 'llama-3.3-70b-versatile',
-'llama-guard-3-8b',
 'llama3-70b-8192',
 'llama3-8b-8192',
+'llama-guard-3-8b',
 'mistral-saba-24b',
-'qwen-2.5-32b',
-'qwen-2.5-coder-32b',
 'qwen-qwq-32b',
 'compound-beta',
-'compound-beta-mini'
+'compound-beta-mini',
+'allam-2-7b'
 ];
 
 const modelSelect = document.getElementById('modelSelect');
